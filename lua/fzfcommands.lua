068c831254f37db6ed208cd40924f119c31db7ce
Josh Watley <watleyjosh@gmail.com>
2024-01-15 23:12:18 +0000

    feat: Ordering commands by most recently used

--- lua/fzfcommands.lua
+++ lua/fzfcommands.lua
@@ -16,6 +16,9 @@ M.history = {}
 -- Local commands from config setup
 M.local_commands = {}
 
+-- Ordered list of commands that we actually want to display
+M.shownList = {}
+
 M.config = {}
 
 function M.setup(config)
@@ -33,6 +36,10 @@ function M.setup(config)
 
     -- Load historic commands
     utils.load_history(M.history)
+
+    -- Build the list of commands
+    -- On init, just combine history and commands
+    M.shownList = utils.combine(M.history, M.commands)
 end
 
 function M.open_fzf_finder(opts)
@@ -40,28 +47,44 @@ function M.open_fzf_finder(opts)
     pickers.new(opts, {
         prompt_title = "Choose a command",
         finder = finders.new_table {
-            results = utils.combine(M.history, M.commands),
+            results = M.shownList,
         },
         sorter = conf.generic_sorter(opts),
         attach_mappings = function(prompt_bufnr, map)
             actions.select_default:replace(function()
                 local selection = actions_state.get_selected_entry()
-                if selection == nil then
-                    local picker = actions_state.get_current_picker(prompt_bufnr)
-                    local prompt = picker:_get_prompt()
-
+                local recent_command = ""
+                local picker = actions_state.get_current_picker(prompt_bufnr)
+                local prompt = picker:_get_prompt()
+                -- If we cant find a selection, then we need to get the prompt
+                if selection == nil or selection[1] ~= prompt and string.len(prompt) > 3 then
                     -- Add to history
                     utils.save_history(M.history, prompt)
 
                     tmux.run(prompt, M.config)
                     actions.close(prompt_bufnr)
-                    return
+                    print(prompt)
+                    recent_command = prompt
+                else
+                    -- Otherwise run the command
+                    actions.close(prompt_bufnr)
+                    print(selection[1])
+                    tmux.run(selection[1], M.config)
+                    recent_command = selection[1]
                 end
 
-                actions.close(prompt_bufnr)
-                tmux.run(selection[1], M.config)
-            end)
+                -- Rebuild the shownList of commands, reordering the most recent command
+                local old_map = M.shownList
+                M.shownList = {}
 
+                table.insert(M.shownList, recent_command)
+
+                for _, command in ipairs(old_map) do
+                    if command ~= recent_command then
+                        table.insert(M.shownList, command)
+                    end
+                end
+            end)
             return true
         end
     }):find()
